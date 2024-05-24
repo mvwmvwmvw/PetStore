0f3571803da9f8f69f17e90500b03ad7ac45c767
mvwmvwmvw <diwxnvxwna@gmail.com>
2024-05-24 19:10:46 +0300

    m

--- petstore.test.js
+++ petstore.test.js
@@ -1,22 +1,55 @@
 const axios = require('axios');
 const URL = 'https://petstore.swagger.io/v2';
 
-describe('Get pet by id', () => {
-  test('Find pet by ID', async () => {
-    const petId = 1; 
-    const response = await axios.get(`${URL}/pet/${petId}`);
-    expect(response.status).toBe(200); 
-    expect(response.data).toHaveProperty('id', petId); 
+describe('Pet API', () => {
+  describe('Get pet by id', () => {
+    test('Find pet by ID', async () => {
+      const petId = 1;
+      const response = await axios.get(`${URL}/pet/${petId}`);
+      expect(response.status).toBe(200);
+      expect(response.data).toHaveProperty('id', petId);
+    });
+
+    test('Invalid ID supplied', async () => {
+      const invalidPetId = "InvalidID";
+      await expect(axios.get(`${URL}/pet/${invalidPetId}`)).rejects.toHaveProperty('response.status', 404);
+    });
+
+    test('Pet not found', async () => {
+      const nonExistentPetId = 7453867495875634;
+      await expect(axios.get(`${URL}/pet/${nonExistentPetId}`)).rejects.toHaveProperty('response.status', 404);
+    });
   });
 
-  test('Invalid ID supplied', async () => {
-    const invalidPetId = "InvalidID";
-    await expect(axios.get(`${URL}/pet/${invalidPetId}`)).rejects.toHaveProperty('response.status', 404); 
-  });
-  
 
-  test('Pet not found', async () => {
-    const nonExistentPetId = 7453867495875634; 
-    await expect(axios.get(`${URL}/pet/${nonExistentPetId}`)).rejects.toHaveProperty('response.status', 404); 
+
+  describe('Find pets by status', () => {
+    test('Find pets by available status', async () => {
+      const status = 'available';
+      const response = await axios.get(`${URL}/pet/findByStatus?status=${status}`);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.data)).toBe(true);
+      expect(response.data.every(pet => pet.status === status)).toBe(true);
+    });
+
+    test('Find pets by pending status', async () => {
+      const status = 'pending';
+      const response = await axios.get(`${URL}/pet/findByStatus?status=${status}`);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.data)).toBe(true);
+      expect(response.data.every(pet => pet.status === status)).toBe(true);
+    });
+
+    test('Find pets by sold status', async () => {
+      const status = 'sold';
+      const response = await axios.get(`${URL}/pet/findByStatus?status=${status}`);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.data)).toBe(true);
+      expect(response.data.every(pet => pet.status === status)).toBe(true);
+    });
   });
+
+
+
+
 });
